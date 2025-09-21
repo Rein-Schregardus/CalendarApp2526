f624518a89491e0a778cf31e3077f357e8f0047c
DaniJ2005 <Danijurjevic2005@gmail.com>
2025-09-21 21:59:41 +0200

    feature/implemented-react-router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,12 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./pages/auth.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
