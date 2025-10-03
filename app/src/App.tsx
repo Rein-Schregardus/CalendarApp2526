15fbc8ee16f4bb90038817ce31201b83fe61d5dc
Rein-Schregardus <rein@schre.nl>
2025-10-03 11:12:00 +0200

    Calendar Component
    Added a calendar component, several helper classes in the new Models folder and a new route /calendar to go to a preview of the calendar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Auth from "./pages/Auth";
+import Auth from "./pages/auth";
+import Calendar from "./components/Calendar";
+import CalendarColumn from "./Models/CalendarColumn";
+import CalendarTimeBlock from "./Models/CalendarTimeBlock";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Auth />} />
+        <Route path="/calendar" element={<Calendar columns={[
+            new CalendarColumn("room 102",[new CalendarTimeBlock(new Date(2025, 8, 30, 13, 30), new Date(2025, 8, 30, 17, 0), "blah blah 2, more talking")])
+        ]}/>}/>
       </Routes>
     </BrowserRouter>
   );
