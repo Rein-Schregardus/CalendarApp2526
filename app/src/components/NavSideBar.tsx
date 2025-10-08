9323d869f34b3153c5091cc0af0139a02658fc5b
Rein-Schregardus <rein@schre.nl>
2025-10-08 11:40:46 +0200

    navbar

--- app/src/components/NavSideBar.tsx
+++ app/src/components/NavSideBar.tsx
@@ -1,11 +1,21 @@
 import styles from "./NavSideBar.module.css";
-import Button from "./Button";
+// import Button from "./Button";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 
 export default function NavSideBar()
 {
+    // const [Open, SetOpen] = useState(true);
     return(
-        <div className={`${styles.NavSideBar}`}>
-            <Button children={<p>toggle</p>}></Button>
-        </div>
+
+        <nav className={`${styles.NavSideBar}`} id="NavSideBar">
+            <div>Logo?</div>
+            {/* <button className="ButtonSideBar" children={<i className='bxr  bx-menu-wider'  ></i>} onClick={() => {SetOpen(!Open)}}></button> */}
+            <ul>
+                <li><Link to="/"><i className='bxr  bx-key'></i>{<h2> Login</h2>}</Link></li>
+                <li><Link to="/"><i className='bxr  bx-calendar-alt'  ></i>{<h2> Calendar</h2>}</Link></li>
+            </ul>
+        </nav>
     )
 }
