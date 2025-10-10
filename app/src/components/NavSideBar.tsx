8b6022b4ba494e7e35c32e6a99182d4b9b216d38
Rein-Schregardus <rein@schre.nl>
2025-10-10 12:11:57 +0200

    refactored navbar
    improved code quality by seperating the buttons from the navbar itself

--- app/src/components/NavSideBar.tsx
+++ app/src/components/NavSideBar.tsx
@@ -1,20 +1,27 @@
-import styles from "./NavSideBar.module.css";
-import { Link } from "react-router-dom";
+import {faUser, faUserClock, faCalendar, faHandshake, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faUserClock, faCalendar, faHandshake, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import NavSideBarLink from "./NavSideBarLink";
 
 export default function NavSideBar()
 {
+    const dividerstyle:string = "border-[#e0e0e0] border-solid border-1 my-1";
     return(
-        <nav className={`${styles.NavSideBar}`} id="NavSideBar">
-            <div>Logo?</div>
-            <ul>
-                <li><Link to="/"><FontAwesomeIcon icon={faUserClock} className={`${styles.Icon}`}/>{<h2> Attendance</h2>}</Link></li>
-                <li><Link to="/"><FontAwesomeIcon icon={faHandshake} className={`${styles.Icon}`}/>{<h2> Events</h2>}</Link></li>
-                <li><Link to="/"><FontAwesomeIcon icon={faCalendar} className={`${styles.Icon}`}/>{<h2> My Calendar</h2>}</Link></li>
-                <li><Link to="/"><FontAwesomeIcon icon={faArrowRightFromBracket} className={`${styles.Icon}`}/>{<h2> Logout</h2>}</Link></li>
-            </ul>
+        <nav id="NavSideBar" className="group flex flex-col py-1 text-[#000000] h-100vh overflow-y-auto overflow-x-hidden w-12 transition-width duration-300 ease-in-out hover:w-38 bg-[#ffffff]">
+
+            <div className="flex justify-between flex-col h-full">
+                <ul>
+                    <div>Logo?</div>
+                    <div className={dividerstyle}></div>
+                    <NavSideBarLink link="/t" text="Attendance" icon={faUserClock}/>
+                    <NavSideBarLink link="/" text="Calendar" icon={faCalendar}/>
+                    <NavSideBarLink link="/" text="Events" icon={faHandshake}/>
+                </ul>
+                <ul>
+                    <div className={dividerstyle}></div>
+                    <NavSideBarLink link="/" text="Profile" icon={faUser}/>
+                    <NavSideBarLink link="/" text="Exit" icon={faArrowRightFromBracket}/>
+                </ul>
+            </div>
         </nav>
     )
 }
