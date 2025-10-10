f72a9766d5f1d7dc12d67008fb209ef9f3e36049
Peter Zoetekouw <pmjzoetekouw@hotmail.com>
2025-10-10 21:38:39 +0200

    runable code

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -4,10 +4,11 @@ import MiniCalendar from '../components/MiniCalendar';
 import UpcomingEvents from '../components/UpcomingEvents';
 import { useState } from 'react';
 import Modal from '../components/Modal/Modal';
+import NavSideBar from '../components/NavSideBar';
 
 import { EventForm } from '../components/Forms/EventForm';
-// import { EventForm } from '../components/Forms/RoomForm';
-// import { EventForm } from '../components/Forms/WorkForm';
+// import { RoomForm } from '../components/Forms/RoomForm';
+// import { WorkForm } from '../components/Forms/WorkForm';
 
 type ModalType = "event" | "room" | "work";
 
@@ -21,59 +22,52 @@ const Home = () => {
   };
 
   return (
+    <div className="h-screen flex">
+      {/* Sidebar */}
+      <NavSideBar />
 
-  <div className="h-screen flex">
-    {/* Sidebar */}
-    <NavSideBar/>
-    <NavSideBar/>
+      {/* Main Content */}
+      <div className="w-5/6 bg-background overflow-y-scroll">
+        <Navbar openCrudModal={openCrudModal} />
 
-    {/* Main Content */}
-    <div className="w-5/6 bg-background overflow-y-scroll">
-      <Navbar />
+        <div className="p-4 flex flex-row gap-4">
+          {/* Left Section */}
+          <div className="w-3/4 flex flex-col gap-8">
+            <div className="w-full h-[800px]">
+              <Schedule />
+            </div>
+          </div>
 
-      <div className="p-4 flex flex-row gap-4">
-        {/* Left Section */}
-        <div className="w-3/4 flex flex-col gap-8">
-        <div className="w-3/4 flex flex-col gap-8">
-          <div className="w-full h-[800px]">
-            <Schedule />
+          {/* Right Section */}
+          <div className="w-1/4 flex flex-col gap-8">
+            <div className="w-full h-[350px]">
+              <MiniCalendar />
+            </div>
+            <div className="w-full h-[375px]">
+              <UpcomingEvents />
+            </div>
           </div>
         </div>
 
-        {/* Right Section */}
-        <div className="w-1/4 flex flex-col gap-8 ">
-          <div className="w-full h-[350px]">
-        <div className="w-1/4 flex flex-col gap-8 ">
-          <div className="w-full h-[350px]">
-            <MiniCalendar />
-          </div>
-          <div className="w-full h-[375px]">
-            <UpcomingEvents />
-          </div>
-          <div className="w-full h-[375px]">
-            <UpcomingEvents />
-          </div>
-        </div>
+        {/* Modal */}
+        {openModal && modalType && (
+          <Modal
+            setOpenModal={setOpenModal}
+            title={
+              modalType === "event"
+                ? "Event"
+                : modalType === "room"
+                ? "Room Reservation"
+                : "Work Schedule"
+            }
+            size="md"
+          >
+            {modalType === "event" && <EventForm />}
+            {/* {modalType === "room" && <RoomForm />}
+            {modalType === "work" && <WorkForm />} */}
+          </Modal>
+        )}
       </div>
-    </div>
-    {/* Modal */}
-      {openModal && modalType && (
-        <Modal
-          setOpenModal={() => setOpenModal(false)}
-          title={
-            modalType === "event"
-              ? "Event"
-              : modalType === "room"
-              ? "Room Reservation"
-              : "Work Schedule"
-          }
-          size="md"
-        >
-          {modalType === "event" && <EventForm />}
-          {/* {modalType === "room" && <RoomForm />}
-          {modalType === "work" && <WorkForm />} */}
-        </Modal>
-      )}
     </div>
   );
 };
