63541811ef59dab4818215ec08c28b104eb65ebe
Rein-Schregardus <rein@schre.nl>
2025-10-30 08:36:04 +0100

    404 page responsiveness
    issue with small screens fixed

--- app/src/pages/NotFoundPage.tsx
+++ app/src/pages/NotFoundPage.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 export default function NotFoundPage()
 {
     return(
-        <div className="flex justify-center center items-center bg-secondary h-screen">
+        <div className="flex justify-center items-center bg-secondary h-screen min-h-fit">
             <div className="max-w-200 bg-primary shadow-md rounded-md h-fit">
-                <h1 className="font-light text-4xl m-2">404 not found</h1>
+                <p className="font-light text-4xl m-2">404 not found</p>
                 <p className="m-2">The page you where looking for could not be found. If you manually enterd the URL concider checking the typing.
                     If you where following a link to a page, we are sorry but the page most likely doesn't exist anymore.</p>
                 <strong className="m-2">The button below will bring you back to the login page</strong>
